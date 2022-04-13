e225ae96b358230b66d7f76c23b413f0cfa8e3e5
Abraiz01 <78402495+Abraiz01@users.noreply.github.com>
2022-04-13 23:25:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ io.sockets.on('connect', (socket) => {
         console.log(gameRooms);
     })
 
+
+
     //get user data
     socket.on('userData', (data) => {
         //save user name in an array
@@ -52,7 +54,7 @@ io.sockets.on('connect', (socket) => {
                 socket.roomName = data.room; // we will add this data to the socket only after we can verify that there is space
                 socket.join(socket.roomName);
                 rooms[socket.roomName]++;
-                // rooms[socket.roomName] = io.sockets.adapter.rooms.get(socket.roomName).size;
+                io.sockets.emit('clientCountLobby', rooms[socket.roomName]);
             } else {
                 delete users[socket.name];
                 console.log(users);
@@ -67,6 +69,7 @@ io.sockets.on('connect', (socket) => {
             rooms[socket.roomName] = 1;  
             // rooms[socket.roomName] = io.sockets.adapter.rooms.get(socket.roomName).size;
             gameRooms[socket.roomName] = 0;
+            io.sockets.emit('clientCountLobby', rooms[socket.roomName]);
         }
 
         console.log(rooms);
@@ -78,7 +81,7 @@ io.sockets.on('connect', (socket) => {
     socket.on('lobbyPositionData', (data) => {
         // console.log(data);
         socket.broadcast.emit('lobbyPositionServer', data);
-        socket.emit('lobbyPositionServer', data);
+        socket.emit('ownPosition', data);
     })
 
     // socket.on('lobbyMousePosition', (data) => {
@@ -102,7 +105,7 @@ io.sockets.on('connect', (socket) => {
                 socket.gameRoomName = data.room; // we will add this data to the socket only after we can verify that there is space
                 socket.join(socket.gameRoomName);
                 gameRooms[socket.gameRoomName]++;
-                // gameRooms[socket.gameRoomName] = io.sockets.adapter.rooms.get(socket.gameRoomName).size;
+                io.sockets.emit('clientCountGame', gameRooms[socket.gameRoomName]);
             } else {
                 delete players[socket.playerName];
                 console.log(users);
@@ -122,25 +125,35 @@ io.sockets.on('connect', (socket) => {
     socket.on('returnData', (data) => {
         gameRooms[data.room]--;
         delete players[data.name];
+        io.sockets.emit('clientCountGame', gameRooms[socket.gameRoomName]);
         console.log(users);
         console.log(players);
         console.log(rooms);
         console.log(gameRooms);
     })
 
-    socket.on('keyData', (data) => {
-        console.log(data);
+    // socket.on('keyData', (data) => {
+    //     console.log(data);
+    // })
+
+    socket.on('mousePositionData', (data) => {
+        socket.broadcast.emit('playerTwoServer', data);
     })
 
     socket.on('playerOnePosition', (data) => {
         // console.log(data);
-        io.sockets.emit('playerOneServerData', data);
+        socket.broadcast.emit('playerTwoServer', data);
     })
 
-    socket.on('playerTwoPosition', (data) => {
+    socket.on('enemyPosition', (data) => {
         // console.log(data);
-        io.sockets.emit('positionDataFromServer', data);
+        socket.broadcast.emit('enemyTwoServer', data);
     })
+
+    // socket.on('playerTwoPosition', (data) => {
+    //     // console.log(data);
+    //     io.sockets.emit('positionDataFromServer', data);
+    // })
 
 })
 
